1a727af666f03b5e2b4d709c862302e0d5039fcd
Udara San <udarassanjeewa@gmail.com>
2024-06-12 22:18:30 +0530

    first commit

---
+++ 03-Loos-Coupling-Agreement/src/Girl.java
@@ -0,0 +1,10 @@
+public class Girl implements Agreement{
+    public void chatWithAmal(){
+        System.out.println("Girl is Chatting");
+    }
+
+    @Override
+    public void chat() {
+        System.out.println("Girl is Chatting");
+    }
+}
